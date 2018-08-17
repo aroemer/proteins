7c0f5dd49af4db29157ab6f35b793b680a514ce3
Audrey ROEMER <aroemer@e2r3p18.42.fr>
2018-08-17 14:38:59 +0200

    icon and launchscreen

--- Proteins/AppDelegate.swift
+++ Proteins/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        Thread.sleep(forTimeInterval: 2.0)
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: ListController())
         return true
     }
 
---
+++ Proteins/ListController.swift
@@ -0,0 +1,24 @@
+//
+//  ListController.swift
+//  Proteins
+//
+//  Created by Audrey ROEMER on 8/17/18.
+//  Copyright © 2018 Audrey ROEMER. All rights reserved.
+//
+
+import UIKit
+
+class ListController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Topics"
+//        setupTableView()
+//        setupNavBar()
+//        handleLogout()
+//        LoginService.shared.delegate = self
+//        addController.delegate = self
+    }
+    
+}
